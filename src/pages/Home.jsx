b7fe3729e96902f564ab2ae1cd6f6c874070ed04
Ponhvorntey Choub <cponhvorntey@gmail.com>
2025-12-26 13:15:29 +0700

    feature(homepage): move  files in components into pages Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Card from '../components/Card'
+import Iphone from '../components/IphoneCard'
+import Ipad from "../components/IpadCard";
+import Twocard  from '../components/Twocard';
+import Slide from "../components/SlideShow";
+import Footer from "../components/Footer"
+import CardGrid from '../components/CardGrid'; 
+
+// import Promotion from "./components/Promotion"
+export default function App() {
+  return (
+    <div className="app-container">
+      <Navbar />
+      {/* <Promotion /> */}
+
+      <Card />
+      <Iphone />
+      <Ipad />
+    
+      <Twocard />
+      <Slide/>
+      <Footer />
+    </div>
+  )
+}
